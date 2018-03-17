8d67dbac2b01c09d84ec29ca26f2dbd05fe0f564
Deniz Tetik <deniz.tetik@oracle.com>
2018-03-16 21:11:44 -0400

    Add subcomponents and use props

--- src/App.js
+++ src/App.js
@@ -4,18 +4,12 @@ import './App.css';
 class App extends Component {
   state = {
     title: 'My Very First Blog Post',
-    content: 'Vim ex mucius tincidunt, at quo justo ceteros facilisis, te erat offendit cum.' + 
-    'Errem oportere cu nam. An tale modus omittantur per, sed fierent detracto ne.' +
-    'Semper inermis reprimique an mei, qui at probo illum accumsan.' +
-    'Id quo quod tincidunt scriptorem, et solet prodesset sea.' + 
-    'Sea an ullum similique interesset.',
+    content: 'Vim ex mucius tincidunt, at quo justo ceteros facilisis, te erat offendit cum.' +
+      'Errem oportere cu nam. An tale modus omittantur per, sed fierent detracto ne.' +
+      'Semper inermis reprimique an mei, qui at probo illum accumsan.' +
+      'Id quo quod tincidunt scriptorem, et solet prodesset sea.' +
+      'Sea an ullum similique interesset.',
     upvotes: 0,
-    titleDirty: 'My Very First Blog Post',
-    contentDirty: 'Vim ex mucius tincidunt, at quo justo ceteros facilisis, te erat offendit cum.' + 
-    'Errem oportere cu nam. An tale modus omittantur per, sed fierent detracto ne.' +
-    'Semper inermis reprimique an mei, qui at probo illum accumsan.' +
-    'Id quo quod tincidunt scriptorem, et solet prodesset sea.' + 
-    'Sea an ullum similique interesset.',
   }
 
   upVote = () => {
@@ -24,63 +18,121 @@ class App extends Component {
     })
   }
 
-  modifyTitle = (event) => {
-    const titleDirty = event.target.value;
-    this.setState({
-      titleDirty: titleDirty,
-    })
-  }
 
-  modifyContent = (event) => {
-    const contentDirty = event.target.value;
-    this.setState({
-      contentDirty: contentDirty,
-    })
-  }
+  savePost = (post) => {
+    const {
+      title,
+      content,
+    } = post;
 
-  savePost = () => {
     this.setState({
-      title: this.state.titleDirty,
-      content: this.state.contentDirty,
+      title,
+      content,
     })
   }
 
   render() {
     return (
       <div className="main">
-        <div className="post">
-          <header className="post__header">
-            <h1 className="post__title">{this.state.title}</h1>
-          </header>
-          <p className="post__body">
-            {this.state.content}
-          </p>
-          <button onClick={this.upVote}>
-            + {this.state.upvotes}
-          </button>
-        </div>
-        <div className="edit-post">
-          <label>Title:</label>
-          <input
-            type="text"
-            name="title"
-            label="Title"
-            value={this.state.titleDirty}
-            onChange={this.modifyTitle}
-          />
-          <label>Content:</label>
-          <textarea
-            className="edit-post__content"
-            type="text"
-            name="content"
-            value={this.state.contentDirty}
-            onChange={this.modifyContent}
-          />
-          <button onClick={this.savePost}>Save Post</button>
-        </div>
+        <Post
+          title={this.state.title}
+          content={this.state.content}
+          upvotes={this.state.upvotes}
+          upVote={this.upVote}
+        />
+        <EditForm
+          title={this.state.title}
+          content={this.state.content}
+          savePost={this.savePost}
+        />
       </div>
     );
   }
 }
 
 export default App;
+
+const Post = (props) => (
+  <div className="post">
+    <header className="post__header">
+      <h1 className="post__title">{props.title}</h1>
+    </header>
+    <p className="post__body">
+      {props.content}
+    </p>
+    <button onClick={props.upVote}>
+      + {props.upvotes}
+    </button>
+  </div>
+);
+
+class EditForm extends Component {
+  state = {
+    title: '',
+    content: '',
+  }
+
+  static getDerivedStateFromProps(props) {
+    const {
+      title,
+      content,
+    } = props;
+
+    return {
+      title,
+      content,
+    }
+  }
+
+  modifyTitle = (event) => {
+    const title = event.target.value;
+    this.setState({
+      title,
+    })
+  }
+
+  modifyContent = (event) => {
+    const content = event.target.value;
+    this.setState({
+      content,
+    })
+  }
+
+  render() {
+    const {
+      savePost
+    } = this.props;
+
+    const {
+      title,
+      content
+    } = this.state;
+
+    return (
+      <div className="edit-post">
+        <label>Title:</label>
+        <input
+          type="text"
+          name="title"
+          label="Title"
+          value={title}
+          onChange={this.modifyTitle}
+        />
+        <label>Content:</label>
+        <textarea
+          className="edit-post__content"
+          type="text"
+          name="content"
+          value={content}
+          onChange={this.modifyContent}
+        />
+        <button
+          onClick={() => {
+            savePost({title, content})
+          }}>
+          Save Post
+        </button>
+      </div>
+    )
+  }
+}
