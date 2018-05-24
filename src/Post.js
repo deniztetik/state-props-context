9ab3236be912cafafc628cde86e04b55e9d41fbf
Deniz Tetik <deniz.tetik@oracle.com>
2018-05-23 20:27:30 -0400

    Break out post into subcomponents

--- src/Post.js
+++ src/Post.js
@@ -2,12 +2,8 @@ import React from 'react';
 
 const Post = (props) => (
   <div className="post">
-    <header className="post__header">
-      <h1 className="post__title">{props.title}</h1>
-    </header>
-    <p className="post__body">
-      {props.content}
-    </p>
+    <PostTitle title={props.title} />
+    <PostContent content={props.content} />
     <button
       className="button"
       onClick={props.addUpVote}>
@@ -17,3 +13,15 @@ const Post = (props) => (
 );
 
 export default Post;
+
+const PostTitle = (props) => (
+  <header className="post__header">
+    <h1 className="post__title">{props.title}</h1>
+  </header>
+);
+
+const PostContent = (props) => (
+  <p className="post__body">
+    {props.content}
+  </p>
+);
