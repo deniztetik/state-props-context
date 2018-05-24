152976799c5013996a42694c010a757d81648fde
Deniz Tetik <deniz.tetik@oracle.com>
2018-05-23 20:20:28 -0400

    Cleanup

--- src/EditForm.js
+++ src/EditForm.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './EditForm.css';
 
 class EditForm extends Component {
   state = {
--- src/Post.js
+++ src/Post.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import './Post.css';
-
 const Post = (props) => (
   <div className="post">
     <header className="post__header">
