f40875efa959181d26c9e2981d6b3faa944e2227
Deniz Tetik <deniz.tetik@oracle.com>
2018-06-19 22:02:19 -0400

    Update to use regular quotes instead of backticks

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import './App.css';
 class App extends Component {
   state = {
     title: 'My Very First Blog Post',
-    content: `Vim ex mucius tincidunt, at quo justo ceteros facilisis, te erat offendit cum. 
-    Errem oportere cu nam. An tale modus omittantur per, sed fierent detracto ne. 
-    Semper inermis reprimique an mei, qui at probo illum accumsan. 
-    Id quo quod tincidunt scriptorem, et solet prodesset sea. 
-    Sea an ullum similique interesset.`,
+    content: 'Vim ex mucius tincidunt, at quo justo ceteros facilisis, te erat offendit cum.' +
+      'Errem oportere cu nam. An tale modus omittantur per, sed fierent detracto ne.' +
+      'Semper inermis reprimique an mei, qui at probo illum accumsan.' +
+      'Id quo quod tincidunt scriptorem, et solet prodesset sea.' +
+      'Sea an ullum similique interesset.',
     upvotes: 0,
   }
 
